c7676de5223ba0ef7a317b1552f8c283df571119
Rémi Delannoy <remi.delannoy@protonmail.com>
2020-07-07 11:55:03 +0200

    fix device_name

--- connect_BT.sh
+++ connect_BT.sh
@@ -4,7 +4,7 @@ if [ "$powered" = "no" ];then
     bluetoothctl -- power on || notify-send 'Bluetooth' "Can't turn the bluetooth on"
 fi
 device_name=$(bluetoothctl -- devices | awk '{for (i=3;i<=NF-1;i++) printf $i" ";print $NF}' | dmenu -i)
-[ ! -z $device_name ] || exit 1
+[ ! -z "$device_name" ] || exit 1
 mac=$(bluetoothctl -- devices | awk "/$device_name/ {print \$2}")
 paired=$(bluetoothctl -- info "$mac" | awk -F ': ' '/Paired/ {print $2}') 
 if [ "$paired" = "no" ];then
