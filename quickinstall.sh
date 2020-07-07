79b407b5821680d0a9a7c6141c7579cab332462f
Rémi Delannoy <remi.delannoy@protonmail.com>
2020-07-07 20:29:36 +0200

    change quickinstall to match the new organisation

--- quickinstall.sh
+++ quickinstall.sh
@@ -7,21 +7,21 @@ curl -fsSLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 #init.vim install
 echo "installing nvim config"
 curl -fsSLo $HOME/.config/nvim/init.vim --create-dirs \
-    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/init.vim 
+    https://raw.githubusercontent.com/remi-delannoy/dotfiles/master/configfiles/init.vim 
 echo "installing nvim plugins"
 nvim -es -u $HOME/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
 #alacritty
 curl -fsSLo $HOME/.config/alacritty/alacritty.yml --create-dirs \
-    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/alacritty.yml
+    https://raw.githubusercontent.com/remi-delannoy/dotfiles/master/configfiles/alacritty.yml
 #fish
 echo "installing fish config"
 curl -fsSL https://get.oh-my.fish | fish
 curl -fsSLo $HOME/.config/fish/conf.d/config.fish --create-dirs \
-    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/config.fish
+    https://raw.githubusercontent.com/remi-delannoy/dotfiles/master/configfiles/config.fish
 #ls color
 echo "installing nice colors for ls"
 curl -fsSLo $HOME/.dircolors/dircolors.ansi-dark --create-dirs \
-    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/dircolors.ansi-dark
+    https://raw.githubusercontent.com/remi-delannoy/dotfiles/master/configfiles/dircolors.ansi-dark
 
 
 
