8c32a7ba4a6a5554f4840a84a13e4f2b73a87478
Rémi Delannoy <remi.delannoy@protonmail.com>
2020-07-07 17:41:00 +0200

    add a script for an easy installation

---
+++ quickinstall.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#vim-plug install
+echo "installing vim-plug"
+curl -fsSLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#init.vim install
+echo "installing nvim config"
+curl -fsSLo $HOME/.config/nvim/init.vim --create-dirs \
+    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/init.vim 
+echo "installing nvim plugins"
+nvim -es -u $HOME/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
+#fish
+echo "installing fish config"
+curl -fsSLo $HOME/.config/fish/conf.d/config.fish --create-dirs \
+    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/config.fish
+#ls color
+echo "installing nice colors for ls"
+curl -fsSLo $HOME/.dircolors/dircolors.ansi-dark --create-dirs \
+    https://raw.githubusercontent.com/remi-delannoy/dotfile/master/dircolors.ansi-dark
+
+
+
